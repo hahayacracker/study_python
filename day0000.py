271dd4695875a5cf6820854b8e491dfb626816f4
hahaya <87736985@qq.com>
2019-04-12 20:02:14 +0800

    add day0000

--- day0000.py
+++ day0000.py
@@ -5,3 +5,23 @@
 # @Site    : 
 # @File    : day0000.py
 # @Software: PyCharm
+
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+import random
+import os
+
+
+def addnum(img):
+    draw = ImageDraw.Draw(img)
+    width, height = img.size
+    # 右上角加数字
+    myfont = ImageFont.truetype(os.getcwd() + '\\font.ttf', size=40)
+    filcolor = '#ff0000'
+    draw.text((width - 50, 0), '99', font=myfont, fill=filcolor)
+    img.save(os.getcwd() +'\\result.jpg', 'jpeg')
+
+
+if __name__ == '__main__':
+    img = Image.open(os.getcwd() + "\\test.png")
+    addnum(img)
